d698a07a8f1b5e6df24cb3ff66ca5b68f34cbcc0
franwindav <45491159+franwindav@users.noreply.github.com>
2019-02-27 21:42:28 +0300

    franwindav

--- scripts/script.js
+++ scripts/script.js
@@ -13,7 +13,7 @@ $("#sliders").ready(function(){
 		return false;
 	});
 	function resizeSliders(){
-		if(window.screen.width <= 370) $("#width").attr("content", "width=370, user-scalable=no");
+		if(window.screen.width <= 400) $("#width").attr("content", "width=370, user-scalable=no");
 		else $("#width").attr("content", "width=device-width, user-scalable=no");
 		var mas = [86, 21, -30, -122];
 		if(screen.width < 550) mas[0]=60;
