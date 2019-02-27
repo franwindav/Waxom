deda2e92fc2fea65138630f7c81cd73fb5d01e6a
franwindav <45491159+franwindav@users.noreply.github.com>
2019-02-27 22:15:20 +0300

    franwindav

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,8 @@ var CountSlider = 0;
 var Blocked = false;
 
 $("#sliders").ready(function(){
-	if(window.screen.width <= 370) $("#width").attr("content", "width=370; user-scalable=no;");
-	else $("#width").attr("content", "width=device-width; user-scalable=no;");
+	if(window.screen.width <= 370) $("#width").attr("content", "width=370, user-scalable=no;");
+	else $("#width").attr("content", "width=device-width, user-scalable=no;");
 	bindSlider();
 	resizeSliders();
 	$(window).resize(function(){
@@ -13,8 +13,8 @@ $("#sliders").ready(function(){
 		return false;
 	});
 	function resizeSliders(){
-		if(window.screen.width <= 400) $("#width").attr("content", "width=370; user-scalable=no;");
-		else $("#width").attr("content", "width=device-width; user-scalable=no;");
+		if(window.screen.width <= 400) $("#width").attr("content", "width=370, user-scalable=no;");
+		else $("#width").attr("content", "width=device-width, user-scalable=no;");
 		var mas = [86, 21, -30, -122];
 		if(screen.width < 550) mas[0]=60;
 		var newHeigth = $("#sliders .slider:nth-of-type(" + (CountSlider + 1) + ")").children().is("img") ? Number.parseFloat($("#sliders .slider:nth-of-type(" + (CountSlider + 1) + ") img").css("height")) : Number.parseFloat($("#sliders .slider:nth-of-type(" + (CountSlider + 1) + ") video").css("height"));
